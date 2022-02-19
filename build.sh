ff0158294656bc9b2a797084c5cae2febe2a4281
Dacoda Strack <dacoda.strack@gmail.com>
2022-02-18 20:52:10 -0800

    rename InterpretStrings to dac-pm-add

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ g++ -ggdb -std=c++20 -c -o build/TicketFromVim.o src/c/TicketFromVim.cpp
 g++ -ggdb -std=c++20 -c -o build/DescriptionGrabber.o src/c/DescriptionGrabber.cpp
 g++ -ggdb -std=c++20 -c -o build/StdinWithUngetStreambuf.o src/c/StdinWithUngetStreambuf.cpp
 g++ -ggdb -std=c++20 -c -o iso-8601-duration-mock.o src/c/iso-8601-duration-mock.cpp
-g++ -ggdb -std=c++20 -o build/interpret-strings src/c/InterpretStrings.cpp $( pkg-config --libs sqlite3 ) iso-8601-duration-mock.o build/TicketFromVim.o build/DescriptionGrabber.o build/StdinWithUngetStreambuf.o
+g++ -ggdb -std=c++20 -o build/dac-pm-add src/c/dac-pm-add.cpp $( pkg-config --libs sqlite3 ) iso-8601-duration-mock.o build/TicketFromVim.o build/DescriptionGrabber.o build/StdinWithUngetStreambuf.o
 
 # ./build/import-tickets < example
